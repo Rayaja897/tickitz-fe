153af261677ea5639d565b829c0e0d61c4f3a9da
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-10-22 23:37:14 +0700

    [ADD] link to change page

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Movie(props) {
     const { poster, title, genres } = props;
 
     return (
         <div className="image-poster">
-            <img src={poster} width="100%" height="300px" alt="poster" />
+            <Link to={`/detail/${title.toLowerCase().split(" ").join("-")}`}>
+                <img src={poster} width="100%" height="300px" alt="poster" />
+            </Link>
             <h5 className="text-center mt-3 single-text">{title}</h5>
             <p className="text-muted text-center text text-muted-mobile" style={{ fontSize: "16px" }}>
                 {genres?.map((item, key) => (
-                <span>{genres.length - 1 === key ? item :`${item}, `}</span>))}    
+                    <span>{genres.length - 1 === key ? item : `${item}, `}</span>))}
             </p>
-            <div className="d-grid mt-2">
-                <button className="btn btn-outline-primary">Details</button>
-            </div>
+            <Link to={`/detail/${title.toLowerCase().split(" ").join("-")}`}>
+                <div className="d-grid mt-2">
+                    <button className="btn btn-outline-primary">Details</button>
+                </div>
+            </Link>
         </div>
     )
 }
