1c1e2b66795079ce372a27408d17954d97f65f75
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-10-30 00:31:51 +0700

    [UPD] can login now

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,66 @@
+import axios from "axios";
 import "../style/Auth.css";
 import "../style/Auth.mobile.css";
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isSucc, setIsSucc] = React.useState(false);
+  const [errMsg, setErrMsg] = React.useState(null);
+
+  React.useEffect(() => {
+    if (localStorage.getItem("token") && localStorage.getItem("profile")) {
+      navigate("/");
+    }
+  });
+
+  const handleLogin = () => {
+    setIsLoading(true);
+    setErrMsg(null);
+
+    axios
+      .post("https://tickitz-be.onrender.com/rayhan/auth/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        const token = response?.data?.data?.token;
+        const profile = response?.data?.data?.result;
+
+        localStorage.setItem("token", token);
+        localStorage.setItem("profile", JSON.stringify(profile));
+        setIsSucc(true);
+      })
+      .catch((error) => {
+        const errEmail = error?.response?.data?.messages?.email?.message;
+        const errPassword = error?.response?.data?.messages?.password?.message;
+
+        setIsSucc(false);
+        setErrMsg(
+          errEmail ??
+            errPassword ??
+            error?.response?.data?.messages ??
+            "Something wrong in our app"
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div id="login_page" style={{ overflow: "hidden" }}>
       <div className="row">
         {/* left content */}
         <div className="col col-md-7">
           <div className="left-content">
-          <Link to="/">
-            <img src="/images/Logo-white.png" className="logo" alt="logo" />
+            <Link to="/">
+              <img src="/images/Logo-white.png" className="logo" alt="logo" />
             </Link>
             <p className="Text">wait, watch, wow!</p>
           </div>
@@ -30,15 +78,28 @@ function Login() {
                 }}
               >
                 Sign in with your data that you entered during
-                <br/>
+                <br />
                 your registration
               </p>
+
+              {isSucc ? (
+                <div className="alert alert-success" role="alert">
+                  Login success, please wait for redirect to our app.
+                </div>
+              ) : null}
+
+              {errMsg ? (
+                <div className="alert alert-danger" role="alert">
+                  {errMsg}
+                </div>
+              ) : null}
 
               <div className="mb-2">
                 <label className="form-label">Email</label>
                 <input
                   className="form-control form-control-lg"
                   placeholder="Write your email"
+                  type="email"
                   onChange={(event) => {
                     setEmail(event.target.value);
                   }}
@@ -50,6 +111,7 @@ function Login() {
                 <input
                   className="form-control form-control-lg"
                   placeholder="Write your password"
+                  type="password"
                   onChange={(event) => {
                     setPassword(event.target.value);
                   }}
@@ -57,11 +119,17 @@ function Login() {
               </div>
 
               <div className="d-grid mb-3">
-                <button className="btn btn-primary">Sign In</button>
+                <button
+                  className="btn btn-primary"
+                  onClick={handleLogin}
+                  disabled={isLoading}
+                >
+                  {isLoading ? "Loading..." : "Sign In"}
+                </button>
               </div>
 
               <p className="text-center">
-              Don’t have an account? {" "}
+                Don’t have an account?{" "}
                 <Link to="/register" style={{ color: "#5F2EEA" }}>
                   Sign Up
                 </Link>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,9 +3,10 @@ import "../style/Auth.css";
 import "../style/Auth.mobile.css";
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
   const [fullName, setFullName] = React.useState("");
   const [phoneNumber, setPhoneNumber] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -13,6 +14,12 @@ function Register() {
   const [isLoading, setIsLoading] = React.useState(false);
   const [isSucc, setIsSucc] = React.useState(false);
   const [errMsg, setErrMsg] = React.useState(null);
+
+  React.useEffect(() => {
+    if (localStorage.getItem("token") && localStorage.getItem("profile")) {
+      navigate("/");
+    }
+  });
 
   const handleRegister = () => {
     setIsLoading(true);
